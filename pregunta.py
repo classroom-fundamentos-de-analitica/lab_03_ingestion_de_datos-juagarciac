e555929fb0fb459b794ee83a4b03518e6e5dff23
Juan Pablo Garcia <juagarciac@unal.edu.co>
2024-05-26 21:53:27 -0500

    segundo intento creo

--- pregunta.py
+++ pregunta.py
@@ -22,27 +22,35 @@ def ingest_data():
 
     for line in lines:
         clean_line = line.strip()
+        clean_line = clean_line.strip("-")
         match = re.match(r'^\s*(\d+)\s+(\d+)\s+([\d,]+)\s+%', clean_line)
         if match:
             if current_cluster:
-                current_cluster['principales_palabras_clave'] = ', '.join(keywords)
+                current_cluster['principales_palabras_clave'] = current_cluster['principales_palabras_clave']+" ".join(keywords)
                 data.append(current_cluster)
                 keywords = []
-
+            palabra = clean_line.split()
+            palabra = palabra[4:]
+            palabra = ' '.join(palabra)
             current_cluster = {
                 'cluster': int(match.group(1)),
                 'cantidad_de_palabras_clave': int(match.group(2)),
-                'porcentaje_de_palabras_clave': float(match.group(3).replace(',', '.'))
-            }
+                'porcentaje_de_palabras_clave': float(match.group(3).replace(',', '.')),
+                'principales_palabras_clave' : palabra
+                }
         
         elif clean_line:
+            if("Cluster  Cantidad de" in clean_line or "Palabras clave" in clean_line):
+                continue
             keywords.extend([word.strip() for word in clean_line.split(',')])
 
     if current_cluster:
-        current_cluster['principales_palabras_clave'] = ', '.join(keywords)
+        current_cluster['principales_palabras_clave'] = current_cluster['principales_palabras_clave']+" ".join(keywords)
         data.append(current_cluster)
 
     df = pd.DataFrame(data)
 
     df.columns = [col.replace(' ', '_').lower() for col in df.columns]
     return df
+
+print( ingest_data())
